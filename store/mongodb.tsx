8fb1e85dfa0bd1b30d25b5d4997bfcd890599335
Maxim <info@twinboards.at>
2023-07-31 15:16:47 +0200

    Add atlas connector for DENO fresh

--- store/mongodb.tsx
+++ store/mongodb.tsx
@@ -3,10 +3,29 @@ import { MongoClient } from "mongo";
 const MONGODB_URL = Deno.env.get("MONGODB_URL") || 'mongodb://127.0.0.1:27017/';
 const database = 'salesy';
 
+
 async function connection() {
     const client = new MongoClient();
     try {
-        await client.connect(MONGODB_URL);
+        if (Deno.env.get("MONGODB_TYPE") == 'atlas') {
+            await client.connect({
+                db: database,
+                tls: true,
+                servers: [
+                    { host: Deno.env.get("MONGODB_HOST1"), port: 27017, },  // primary
+                    { host: Deno.env.get("MONGODB_HOST2"), port: 27017, },  // secondary
+                    { host: Deno.env.get("MONGODB_HOST3"), port: 27017, },  // secondary
+                ],
+                credential: {
+                    username: Deno.env.get("MONGODB_USER"),
+                    password: Deno.env.get("MONGODB_PASSWORD"),
+                    db: database,
+                    mechanism: "SCRAM-SHA-1",
+                },
+            });
+        } else {
+            await client.connect(MONGODB_URL);
+        }
         return client.database(database);
     } catch (error) {
         console.error(error);
