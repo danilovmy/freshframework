bf31592ee10cd19ff12b64f2abf0aac9a958c915
Maxim <info@twinboards.at>
2023-08-22 07:44:41 +0200

    update onsubmit

--- components/messageloader.tsx
+++ components/messageloader.tsx
@@ -1,6 +1,6 @@
 export default function messageLoader({loader}) {
     return (
-        <form method="POST" onSubmit={loader}>
+        <form method="POST" onsubmit={loader}>
             <button type="submit" name="loader" class="bg-grey-light border rounded p-4 shadow-md">
                 load previous messages
             </button>
